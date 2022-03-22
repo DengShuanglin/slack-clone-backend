0e6d4f6ae1ef580442b4cbfa1c9aeae6b6ae6e94
lhj <1871731528@qq.com>
2022-03-22 15:20:02 +0800

    feat(auth): add jwt

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,32 @@
+/**
+ * @file Auth module
+ * @module module/auth/module
+ * @author Name6
+ */
+
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import jwt from 'jsonwebtoken';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import * as APP_CONFIG from '@/app.config';
+import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      privateKey: APP_CONFIG.AUTH.jwtTokenSecret as jwt.Secret,
+      signOptions: {
+        expiresIn: APP_CONFIG.AUTH.expiresIn as number,
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, UserModule],
+  exports: [AuthService],
+})
+export class AuthModule {}
