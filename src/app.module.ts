72d053d8998b91f75245c74d7b1ad1a6483dbdcf
lhj <1871731528@qq.com>
2022-03-22 15:15:20 +0800

    feat(guards): add auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+/**
+ * @file App module
+ * @module app/module
+ * @author Name6
+ */
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './processors/database/database.module';
+import { HelperModule } from './processors/helper/helper.module';
+import { CacheModule } from './processors/cache/cache.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/auth.guard';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, HelperModule, CacheModule, AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,46 @@
+/**
+ * @file JwtAuth guard and permission authorize
+ * @module guard/auth
+ * @author Name6
+ */
+
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { HttpUnauthorizedError } from '@/errors/unauthorized.error';
+import { UNDEFINED } from '@/constants/value.constant';
+import { Reflector } from '@nestjs/core';
+import { AUTHORIZE_KEY_METADATA } from '@/constants/meta.constant';
+
+/**
+ * @class JwtAuthGuard
+ * @classdesc 检验规则：Token 是否存在 -> Token 是否在有效期内 -> Token 解析出的数据是否对的上
+ * @example ```@UseGuards(JwtAuthGuard)```
+ */
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    // 如果加入@Authorize则跳过校验
+    const authorize = this.reflector.get<boolean>(
+      AUTHORIZE_KEY_METADATA,
+      context.getHandler(),
+    );
+
+    if (authorize) return true;
+    return super.canActivate(context);
+  }
+
+  /**
+   * @method handleRequest
+   * @description 如果解析出的数据对不上，则判定为无效，并进行权限验证
+   */
+  handleRequest(error, authInfo, errInfo) {
+    if (authInfo && !error && !errInfo) {
+      return authInfo;
+    } else {
+      throw error || new HttpUnauthorizedError(UNDEFINED, errInfo?.message);
+    }
+  }
+}
